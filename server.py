7bea4ba68555d8f9e72c9d65682c5312601e876b
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-10-24 22:17:19 +0200

    Final Pep8

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ import json
 from time import time, gmtime, strftime
 
 
-def register (line_decod, dicc_usuarios, dicc, client_infor):
+def regist(line_decod, dicc_usuarios, dicc, client_infor):
     """
     Registro de usuarios
     """
@@ -19,14 +19,13 @@ def register (line_decod, dicc_usuarios, dicc, client_infor):
     expiracion = int(line_decod.split()[4])
     expira = int(time()) + expiracion
     dicc_usuarios['address'] = client_infor[0]
-    
+
     #coje el dia al que estamos y la hora y lo almacena en tiempo_ahora
     tiempo_ahora = int(time())
     strf_ahora = strftime('%Y-%m-%d %H:%M:%S', gmtime(tiempo_ahora))
     tiempo_expira = expiracion + tiempo_ahora
     strf_expira = strftime('%Y-%m-%d %H:%M:%S', gmtime(tiempo_expira))
-    
- 
+
     dicc_usuarios["expira"] = strf_expira + ' + ' + str(expiracion)
 
 #casos para que borre la direccion del fichero
@@ -35,10 +34,11 @@ def register (line_decod, dicc_usuarios, dicc, client_infor):
             del dicc[direccion]
     elif '@' in direccion:
         dicc[direccion] = dicc_usuarios
-    
+
     for usuario in dicc:
-        if int(time())> expira:
+        if int(time()) > expira:
             deldicc[direccion]
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -46,7 +46,6 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
 
     dicc = {}
-
 
     def handle(self):
 
@@ -58,22 +57,21 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             line_decod = line.decode('utf-8')
             if len(line_decod) >= 2:
                 if line_decod.split()[0].upper() == 'REGISTER':
-                    register(line_decod, dicc_usuarios, self.dicc, client_infor)
+                    regist(line_decod, dicc_usuarios, self.dicc, client_infor)
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     self.register2json()
             print("El cliente nos manda " + line_decod)
-            
+
             if not line:
                 break
-                
+       
     def register2json(self):
         """
         Registro de los usuarios en un json
         """
-        fichero_json= json.dumps(self.dicc)
-        with open ('registro.json', 'w') as fichero_json:
-            json.dump(self.dicc, fichero_json, sort_keys = True, indent = 4)
-
+        fichero_json = json.dumps(self.dicc)
+        with open('registro.json', 'w') as fichero_json:
+            json.dump(self.dicc, fichero_json, sort_keys=True, indent=4)
 
     def json2register(self):
         """
